80f557de86b3248e1faed7a566b5302b48fd6702
Satyavrat Mishra <20je0874@mech.iitism.ac.in>
2024-06-27 01:53:39 +0530

    added different routes and also created the nav-bar and given some styling

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-Links-container">
+          <Link className="nav-Link" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-Link" to="/signIn">
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
---
+++ src/routes/sign-in/signIn.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>I am the Sign IN Page</div>;
+};
+
+export default SignIn;
